4b1a66b6acadd23e09cbef4af591fdc9a53fa960
Labeeb Maqsood <labeeb.maqsood@murcul.tech>
2020-07-19 19:52:42 +0500

    Functions tests solved with es6 excluding currying n partial

--- app/functions.js
+++ app/functions.js
@@ -2,31 +2,40 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn(...arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return (arg) => {
+      return str + ', ' + arg;
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+    let response = [];
+    arr.forEach(val => {
+      response.push(() => fn(val)); // inserting reference to function on each index
+    })
+    return response;
   },
 
   partial: function(fn, str1, str2) {
 
   },
 
-  useArguments: function() {
+  useArguments: function(...args) {
+    return args.reduce((curr, acc) => {
+      return curr + acc
+    },0);
 
   },
 
-  callIt: function(fn) {
-
+  callIt: function(fn, ...args) {
+    return fn.apply(null, args);
   },
 
   partialUsingArguments: function(fn) {
